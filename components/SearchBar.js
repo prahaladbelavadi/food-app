95ae8386708ea3a8eca10f8dbbe72c4cfd7ca4db
Prahalad Belavadi <github@belavadi.com>
2020-07-14 01:25:56 +0530

    fix bug

---
+++ components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+export default SearchBar = () => {
+  return (
+    <View background>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+  },
+});
